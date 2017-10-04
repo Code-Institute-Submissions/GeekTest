29ccf11d3d3c8961b7fd29cf32ce8733c7ae0fc5
stephen ryan <stephen.ryan11@gmail.com>
2017-10-04 14:42:07 +0100

    heroku

--- settings/staging.py
+++ settings/staging.py
@@ -3,33 +3,18 @@ import dj_database_url
 
 DEBUG = False
 
-# Load the ClearDB connection details from the environment variable
 DATABASES = {
-    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
-
+DATABASES['default'] = dj_database_url.parse(CLEAR_DB_URL)
 
 # Paypal environment variables
 PAYPAL_NOTIFY_URL = 'https://geekshtop.herokuapp.com'
 PAYPAL_RECEIVER_EMAIL = 'geeknshtuff@outlook.com'
 
-SITE_URL = 'https://geekshtop.herokuapp.com'
-ALLOWED_HOSTS.append('geekshtop.herokuapp.com')
 
-# Log DEBUG information to the console
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
+
