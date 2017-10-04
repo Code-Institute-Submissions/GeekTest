795bdc4016988bb760f3ccad5b5601163b5371f3
stephen ryan <stephen.ryan11@gmail.com>
2017-10-04 16:57:23 +0100

    heroku

--- settings/base.py
+++ settings/base.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: Enter your own SECRET_KEY here
 SECRET_KEY = 'mykey'
 
-ALLOWED_HOSTS = ['127.0.0.1','cbbd8856.ngrok.io', ]
+ALLOWED_HOSTS = ['127.0.0.1','cbbd8856.ngrok.io', 'geekstuff.herokuapp.com']
 SITE_ID = 2
 
 # Application definition
