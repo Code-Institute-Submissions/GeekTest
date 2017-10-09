ed8cfbb5b1f96a9a0012009646d61baccb3524b7
stephen ryan <stephen.ryan11@gmail.com>
2017-10-09 17:20:46 +0100

    paypal

--- settings/staging.py
+++ settings/staging.py
@@ -17,8 +17,8 @@ CLEAR_DB_URL = os.environ.get("CLEARDB_DATABASE_URL", "")
 DATABASES['default'] = dj_database_url.parse(CLEAR_DB_URL)
 
 # Paypal environment variables
-PAYPAL_NOTIFY_URL = 'http://geekstuff.herokuapp.com'
-PAYPAL_RECEIVER_EMAIL = 'http://geeknshtuff@outlook.com'
+PAYPAL_NOTIFY_URL = 'geekstuff.herokuapp.com'
+PAYPAL_RECEIVER_EMAIL = 'geeknshtuff@outlook.com'
 
 SITE_URL = 'http://geekstuff.herokuapp.com'
 ALLOWED_HOSTS.append('geekstuff.herokuapp.com')
