e354ade5b7237aba93d01daf080a9d95042377a8
stephen ryan <stephen.ryan11@gmail.com>
2017-10-23 20:08:51 +0100

    fix

--- project/settings.py
+++ project/settings.py
@@ -167,3 +167,8 @@ MEDIA_URL = '/media/'
 DISQUS_WEBSITE_SHORTNAME = 'geekshop'
 
 SITE_ID = 2
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'stephen.ryan11@gmail.com'
+EMAIL_HOST_PASSWORD = 'Gutantag1'
+EMAIL_PORT = 587
