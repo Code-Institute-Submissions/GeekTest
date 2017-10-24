b472db725f7ad13a1f98e50023d267ae863ff7db
stephen ryan <stephen.ryan11@gmail.com>
2017-10-24 16:50:31 +0100

    contact

--- accounts/views.py
+++ accounts/views.py
@@ -93,10 +93,10 @@ def contact(request):
             contact_email = request.POST.get('contact_email' , '')
 
 
-            subject = "Thanks for the email, We will get back to you soon."
+            subject = "Geekshop"
             from_email = settings.EMAIL_HOST_USER
             to_email = [contact_email]
-            message = """welcome"""
+            message = """Thanks for the message we will get back to you soon."""
             send_mail(subject=subject, from_email=from_email, recipient_list=to_email, message=message, fail_silently=False)
 
             return redirect('contact_thanks')
