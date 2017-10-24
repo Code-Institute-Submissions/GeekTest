db939fbad6459856f58034d35502114e5e0ab5a0
stephen ryan <stephen.ryan11@gmail.com>
2017-10-24 19:59:26 +0100

    email

--- accounts/views.py
+++ accounts/views.py
@@ -7,10 +7,13 @@ from django.contrib.auth.decorators import login_required
 
 from django.conf import settings
 from accounts.forms import ContactForm
-from django.core.mail import EmailMessage
-from django.core.mail import send_mail
 
+from django.core.mail import send_mail, EmailMultiAlternatives
 from django.template.loader import get_template
+
+
+
+
 
 
 
@@ -91,13 +94,20 @@ def contact(request):
         if form.is_valid():
 
             contact_email = request.POST.get('contact_email' , '')
+            contact_name = request.POST.get('contact_name', '')
 
 
             subject = "Geekshop"
             from_email = settings.EMAIL_HOST_USER
             to_email = [contact_email]
-            message = """Thanks for the message we will get back to you soon."""
-            send_mail(subject=subject, from_email=from_email, recipient_list=to_email, message=message, fail_silently=False)
+           # message = """Thanks for the message we will get back to you soon."""
+           # send_mail(subject=subject, from_email=from_email, recipient_list=to_email, message=message, fail_silently=False)
+            with open(settings.BASE_DIR + "/templates/contacts/contacts_email.txt") as f:
+                sign_message = f.read()
+                message = EmailMultiAlternatives(subject = subject, body = sign_message, from_email=from_email, to=to_email)
+                html_template = get_template("contacts/contacts_email.html").render()
+                message.attach_alternative(html_template, "text/html")
+                message.send()
 
             return redirect('contact_thanks')
 
